04da8619b60d8cec8db8c8c1f019b3ae7e6cd46b
mikesam <mikesam37@gmail.com>
2024-09-15 04:41:08 +0100

    Remove .html images from css files, refactor store.html to store-extended/page.jsx

---
+++ app/store-extended/layout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "/public/css/bootstrap.min.css";
+import "/public/css/animate.css";
+import "/public/css/owl.carousel.css";
+import "/public/css/font-awesome.css";
+import "/public/css/themify-icons.css";
+import "/public/css/flaticon.css";
+import "/public/revolution/css/layers.css";
+import "/public/revolution/css/settings.css";
+import "/public/css/prettyPhoto.css";
+import "/public/css/shortcodes.css";
+import "/public/css/main.css";
+import "/public/css/responsive.css";
+
+export const metadata = {
+  title: "Greenagrichain | Store",
+  keywords: ["Greenagrichain", "Greenagrichain", "Technology"],
+  authors: [{ name: "TheGreenChain" }],
+};
+
+export default function layout({ children }) {
+  return children;
+}
